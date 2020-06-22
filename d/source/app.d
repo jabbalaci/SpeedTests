04eade7e6c7fabcdbaefef789d17e1d5254457ab
adnan338 <relay.dev.adnan@protonmail.com>
2020-06-23 02:19:58 +1000

    add D

---
+++ d/source/app.d
@@ -0,0 +1,41 @@
+private:
+bool isMünchausen(ulong number, ulong[10] cache)
+{
+	int total;
+	for (ulong n = number; n > 0; n /= 10)
+	{
+		const auto digit = n % 10;
+		total += cache[digit];
+		if (total > number)
+			return false;
+	}
+
+	return number == total;
+}
+
+ulong[10] getCache()
+{
+	ulong[10] result;
+	import std.math : pow;
+
+	foreach (i; 1 .. 10)
+		result[i] = i.pow(i);
+	return result;
+}
+
+public:
+void main()
+{
+	enum cache = getCache();
+	import core.stdc.stdio : printf;
+	import std.parallelism : parallel;
+	import std.range : iota;
+
+	foreach (n; parallel(iota(440_000_000)))
+	{
+		if ((n > 0) && n % 1_000_000 == 0)
+			printf("# %zu\n", n);
+		if (isMünchausen(n, cache))
+			printf("%zu\n", n);
+	}
+}

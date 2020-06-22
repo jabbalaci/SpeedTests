c578562e1e0240948f14ea3118797d6136b2d55f
Danil Yarantsev <tiberiumk12@gmail.com>
2020-06-22 20:06:05 +0300

    Use uint32 for Nim (as in Rust)

--- nim/main.nim
+++ nim/main.nim
@@ -2,19 +2,18 @@ import
   math,
   strformat
 
-func get_cache(): array[10, int] =
-  result[0] = 0;
-  for i in 1 .. 9:
+func get_cache(): array[10, uint32] =
+  for i in 1'u32 .. 9:
     result[i] = i ^ i
 
 const
-  MAX = 440_000_000
-  cache: array[10, int] = get_cache()
+  MAX = 440_000_000'u32
+  cache = get_cache()
 
-func is_munchausen(number: int): bool =
+func is_munchausen(number: uint32): bool =
   var
     n = number
-    total = 0
+    total = 0'u32
 
   while n > 0:
     let digit = n mod 10
@@ -26,7 +25,7 @@ func is_munchausen(number: int): bool =
   total == number
 
 proc main() =
-  for i in 1 ..< MAX:
+  for i in 1'u32 ..< MAX:
     if (i > 0) and (i mod 1_000_000 == 0):
       echo &"# {i}"
     if is_munchausen(i):

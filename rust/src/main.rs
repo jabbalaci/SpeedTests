6e5088371f2aca84fd3279eef9d3a5ea09449c54
Jabba Laci <jabba.laci@gmail.com>
2020-06-24 13:40:11 +0200

    Rust restored

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,34 +1,42 @@
-use std::io::Write;
-const MAX: i32 = 440_000_000;
+const N: i32 = 440_000_000;
 
-fn is_munchausen(number: i32, cache: &[i32]) -> bool {
+
+fn is_munchausen(number: i32, cache: &[i32; 10]) -> bool
+{
     let mut n = number;
     let mut total = 0;
 
-    while n > 0 {
+    while n > 0
+    {
         let digit = n % 10;
         total += cache[digit as usize];
         if total > number {
             return false;
         }
-        n /= 10;
+        n = n / 10;
     }
+
     number == total
 }
 
-fn get_cache() -> Vec<i32> {
-    (0..10).map(|n: i32| n.pow(n as u32)).collect()
+fn get_cache() -> [i32; 10]
+{
+    let mut cache = [0; 10];    // init. with 0s
+    // cache[0] == 0
+    for n in 1 ..= 9 {
+        cache[n] = i32::pow(n as i32, n as u32);
+    }
+    // println!("# {:?}", cache);
+    cache
 }
 
-fn main() {
-    let cache = get_cache();
+fn main()
+{
+    let cache: [i32; 10] = get_cache();
 
-    let stream = std::io::stdout();
-    let mut lock = stream.lock();
-
-    for n in 0..MAX + 1 {
+    for n in 0 .. N {
         if is_munchausen(n, &cache) {
-            let _ = lock.write_fmt(format_args!("{}\n", n));
+            println!("{}", n);
         }
     }
 }

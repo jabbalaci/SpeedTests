a031e84ee1b541f63bf955f38fee9cda02043edd
Mason Protter <mason.protter@icloud.com>
2021-10-10 09:54:17 -0700

    improve Julia runtime

--- julia/main.jl
+++ julia/main.jl
@@ -1,15 +1,8 @@
 #!/usr/bin/env julia
 
-N = 440_000_000
+const N = Ref(440_000_000)
 
-function get_cache()
-    # 1^1, 2^2, ..., 9^9
-    cache = [0, 0, 0, 0, 0, 0, 0, 0, 0]
-    for i in 1:9
-        cache[i] = i ^ i
-    end
-    return cache
-end
+get_cache() = ntuple(i -> i^i, 9)
 
 function is_munchausen(number, cache)
     n = number
@@ -18,7 +11,7 @@ function is_munchausen(number, cache)
     while n > 0
         digit = mod(n, 10)
         if digit > 0
-            total += cache[digit]
+            @inbounds total += cache[digit]
         end
         if total > number
             return false
@@ -32,7 +25,7 @@ end
 function main()
     cache = get_cache()
 
-    for n in 0:N
+    for n in 0:N[]
         if is_munchausen(n, cache)
             println(n)
         end

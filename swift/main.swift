c3e07e152e717a752c22623d592dcd66822c6f98
Jabba Laci <jabba.laci@gmail.com>
2024-02-09 19:54:59 +0100

    Swift

--- swift/main.swift
+++ swift/main.swift
@@ -1,15 +1,21 @@
-let MAX : UInt32 = 440_000_000
+// let MAX: UInt32 = 10_000
+let MAX: UInt32 = 440_000_000
 
-var cache : [UInt32] = [0, 1, 4]
+var cache: [UInt32] = [0]
 
-func is_munchausen(number : UInt32) -> Bool
-{
+for i in 1...9 {
+    var power = 1
+    for _ in 0..<i { power *= i }
+    cache.append(UInt32(power))
+}
+
+func is_munchausen(_ number: UInt32) -> Bool {
     var n = number
-    var total : UInt32 = 0
-    while (n > 0)
-    {
+    var total: UInt32 = 0
+
+    while n > 0 {
         total += cache[Int(n % 10)]
-        if (total > number) {
+        if total > number {
             return false
         }
         n = n / 10
@@ -18,13 +24,10 @@ func is_munchausen(number : UInt32) -> Bool
     return total == number
 }
 
-for i in 3...9 {
-    var power = 1
-    for _ in 0..<i { power *= i }
-    cache.append(UInt32(power))
+func main() {
+    for n in 0..<MAX where is_munchausen(n) {
+        print(n)
+    }
 }
 
-for k in 0..<MAX where is_munchausen(number:k)
-{
-    print(k)
-}
+main()
